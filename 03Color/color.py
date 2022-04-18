b0e58670e543ef09da2c852b27627e4f52cb523d
t-murash <takamura1978@gmail.com>
2022-04-18 16:10:53 +0900

    03Color

--- 03Color/color.py
+++ 03Color/color.py
@@ -42,11 +42,11 @@ pipeline.add_to_scene()
 
 from ovito.vis import ParticlesVis
 particle_vis=pipeline.source.data.particles.vis
-particle_vis.radius=0.3
+particle_vis.radius=0.4
 
 from ovito.vis import BondsVis
 bond_vis=pipeline.source.data.particles.bonds.vis
-bond_vis.width=0.2
+bond_vis.width=0.6
 
 cell_vis=pipeline.source.data.cell.vis
 cell_vis.rendering_color=(1.0,1.0,1.0)
